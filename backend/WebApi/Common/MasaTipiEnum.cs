b69f6b6a09ac3cfa91e67311eed426e82d66a3d6
Berk Akın <berkakin99@hotmail.com>
2024-10-22 15:11:11 +0300

    cache update

---
+++ backend/WebApi/Common/MasaTipiEnum.cs
@@ -0,0 +1,5 @@
+public enum MasaTipi
+{
+    Small = 1, // "Küçük 2"
+    Large = 2  //"Büyük 4"
+}
--- backend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ backend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+796aeb56413397c4fe4219955e76ab7bfab102b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7ddfd84f8d68ba47131733b4d060760cebbb878")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
