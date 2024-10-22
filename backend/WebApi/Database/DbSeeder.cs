3057423e75c78559c5c0882a8fa29c90902bf67a
Berk Akın <berkakin99@hotmail.com>
2024-10-22 17:26:21 +0300

    FEAT: Created DBSeeder prod

---
+++ backend/WebApi/Database/DbSeeder.cs
@@ -0,0 +1,46 @@
+using WebApi.Database;
+using WebApi.Entities;
+
+public class DbSeeder
+{
+    public static void Seed(RestaurantDbContext context)
+    {
+        if (!context.Restaurants.Any())
+        {
+            var restaurant = new Restaurant
+            {
+                Name = "Örnek Restoran",
+                Address = "Örnek Adres",
+                PhoneNumber = "123456789",
+                Email = "example@example.com",
+            };
+
+            context.Restaurants.Add(restaurant);
+            context.SaveChanges();
+
+            // 10 adet küçük masa
+            for (int i = 0; i < 10; i++)
+            {
+                context.Tables.Add(new Table
+                {
+                    TableType = 1,
+                    Capacity = 2,  // Küçük masa kapasitesi
+                    RestaurantId = restaurant.Id
+                });
+            }
+
+            // 10 adet büyük masa
+            for (int i = 0; i < 10; i++)
+            {
+                context.Tables.Add(new Table
+                {
+                    TableType = 2,
+                    Capacity = 4,  // Büyük masa kapasitesi
+                    RestaurantId = restaurant.Id
+                });
+            }
+
+            context.SaveChanges(); // Değişiklikleri kaydedin
+        }
+    }
+}
