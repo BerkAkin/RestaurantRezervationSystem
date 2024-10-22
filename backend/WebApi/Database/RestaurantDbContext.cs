445f3bfed19a024a4a3d7237d34d7cd73ae43a5a
Berk Akın <berkakin99@hotmail.com>
2024-10-22 17:26:39 +0300

    FEAT: Relation between tables and restaurants defined

--- backend/WebApi/Database/RestaurantDbContext.cs
+++ backend/WebApi/Database/RestaurantDbContext.cs
@@ -11,5 +11,21 @@ namespace WebApi.Database
         }
 
         public DbSet<Table> Tables { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Table>()
+                .HasOne(t => t.Restaurant)
+                .WithMany(r => r.Tables)
+                .HasForeignKey(t => t.RestaurantId);
+
+            modelBuilder.Entity<Restaurant>()
+               .HasMany(r => r.Tables)
+               .WithOne(t => t.Restaurant)
+               .HasForeignKey(t => t.RestaurantId);
+        }
     }
 }
