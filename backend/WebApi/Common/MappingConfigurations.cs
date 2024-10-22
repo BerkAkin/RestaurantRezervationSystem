06ff6c3a0e22581d7ba5a7c6f4fcb5f5b8cc4820
Berk Akın <berkakin99@hotmail.com>
2024-10-22 20:08:34 +0300

    FEAT: Implemented resaturant detail vie mapping setting

--- backend/WebApi/Common/MappingConfigurations.cs
+++ backend/WebApi/Common/MappingConfigurations.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using WebApi.Application.RestaurantOperations.Queries.GetRestaurant;
+using WebApi.Application.RestaurantOperations.Queries.GetRestaurantDetails;
 using WebApi.Entities;
 
 namespace WebApi.Common
@@ -9,6 +10,16 @@ namespace WebApi.Common
         public MappingConfigurations()
         {
             CreateMap<Restaurant, GetRestaurantViewModel>();
+            CreateMap<Restaurant, GetRestaurantDetailViewModel>()
+            .ForMember(dest => dest.Tables, opt => opt.MapFrom(src => src.Tables.Select(
+                t => new RestaurantTablesViewModel
+                {
+                    Id = t.Id,
+                    Capacity = t.Capacity,
+                    IsAvailable = t.IsAvailable,
+                    TableType = t.TableType
+                }
+            )));
         }
     }
 }
